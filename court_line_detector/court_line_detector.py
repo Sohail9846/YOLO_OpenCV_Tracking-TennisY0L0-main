6f7b9c6c3f566e45af92a773b671d924dd17812c
Sohail9846 <sohailansari9846@gmail.com>
2026-01-07 23:40:49 +0530

    Update court_line_detector.py

--- court_line_detector/court_line_detector.py
+++ court_line_detector/court_line_detector.py
@@ -14,7 +14,8 @@ class CourtLineDetector:
             transforms.Resize((224, 224)),
             transforms.ToTensor(),
             transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
-        ])
+        ]
+                                           )
 
     def predict(self, image):
 
